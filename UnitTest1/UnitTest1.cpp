a329ec8f6aaa56d61d5aa99c4159310e51a99a49
Aquat1c <49980958+Aquat1c@users.noreply.github.com>
2020-12-01 12:52:30 +0200

    Add files via upload

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,28 @@
+
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "../lab8.1/lab8.1.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest812
+{
+	TEST_CLASS(UnitTest812)
+	{
+	public:
+
+		TEST_METHOD(TestMethod1)
+#define _CRT_SECURE_NO_WARNINGS
+		{
+
+
+			int i = 0;
+			char str[10] = { '+','\0' };
+
+			int m;
+			m = Count(str, 0);
+			Assert::AreEqual(m, 0);
+		}
+	};
+}
+
